8924bf95fcc96441565966f0f645451c3bc26dbc
Hauke Jaeger <hauke.jaeger@yegair.io>
2021-01-21 07:55:38 +0100

    chore(github): build and test using GitHub Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,19 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm").version("1.4.21")
     `java-library`
+    id("com.diffplug.spotless").version("5.9.0")
+}
+
+fun getJavaLanguageVersion(): JavaLanguageVersion {
+    return when (val configuredVersion = System.getProperty("javaLanguageVersion", "1.8")) {
+        "1.8" -> JavaLanguageVersion.of("8")
+        else -> JavaLanguageVersion.of(configuredVersion)
+    }
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(getJavaLanguageVersion())
     }
 }
 
@@ -29,4 +37,16 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+spotless {
+
+    kotlin {
+        target("**/*.kt")
+        ktlint("0.40.0")
+    }
+
+    java {
+        googleJavaFormat("1.7").aosp()
+    }
 }
